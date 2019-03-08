41138f0b5f0b0636e8c3a3bffda257f82d78a3c6
Hu <ch4@illinois.edu>
2019-03-08 10:30:38 -0600

    Added paging files

---
+++ student-distrib/paging.c
@@ -0,0 +1,29 @@
+#include "paging.h"
+#include "paging_assem.h"
+
+void initialize_page() {
+	int j;
+	
+	/* set all values in all page directory and table entries to 0 */
+	for (j = 0; j < NUM_ENTRIES; j++) {
+		directory_entry_array[j].val = 0;
+		table_entry_array[j].val = 0;
+	}
+	
+	/* video memory is at 0xB8000, in other words in index 0 of the directory_entry_array, and in index 23 of the table_entry_array */
+	directory_entry_array[0].present = 1;
+	directory_entry_array[0].read_write = 1;
+	directory_entry_array[0].page_size = 0;
+	directory_entry_array[0].p_table_addr = table_entry_array;
+	table_entry_array[VIDEO_ADDR/4_KB].present = 1;
+	table_entry_array[VIDEO_ADDR/4_KB].read_write = 1;
+	table_entry_array[VIDEO_ADDR/4_KB].p_base_address = VIDEO_ADDR/4_KB;	// address has to be mapped from 4 kB
+
+	/* kernel space is from 4MB-8MB, in other words in index 1 of the directory_entry_array */
+	directory_entry_array[1].present = 1;
+	directory_entry_array[1].read_write = 1;
+	directory_entry_array[1].page_size = 1;	// kernel space is 4 MB
+	directory_entry_array[1].p_table_addr = KERNEL_ADDR>>3;	// address has to be mapped from 4 kB
+	
+	enable_paging(directory_entry_array);
+}
---
+++ student-distrib/paging_assem.h
@@ -0,0 +1,4 @@
+#ifndef _PAGING_ASSEM
+#define _PAGING_ASSEM
+
+extern void enable_paging(page_directory_entry_t* pointer);
