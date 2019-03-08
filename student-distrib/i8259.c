ba874e45ccfe58e1e5c116a7253943027aad3713
Jason Hahm <hahm95@gmail.com>
2019-03-08 15:37:14 -0600

    PIC code revision

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -11,7 +11,7 @@ uint8_t slave_mask;  /* IRQs 8-15 */
 
 /* Initialize the 8259 PIC */
 void i8259_init(void) {
-    master_mask = inb(MASTER_DATA);     //save masks
+    master_mask = inb(MASTER_DATA);     //save current masks
     slave_mask = inb(SLAVE_DATA);
 
     outb(MASTER_COMMAND, ICW1);         //enter init mode (0x11)
@@ -20,7 +20,7 @@ void i8259_init(void) {
     outb(MASTER_DATA, ICW2_MASTER);     //set vector offsets (0x20 and 0x28)
     outb(SLAVE_DATA, ICW2_SLAVE);
 
-    outb(MASTER_DATA, ICW3_MASTER);     //tell master that slave is at IRQ2
+    outb(MASTER_DATA, ICW3_MASTER);     //tell master that slave is at IRQ2 (0x04)
     outb(SLAVE_DATA, ICW3_SLAVE);       //tell slave that it has master
 
     outb(MASTER_DATA, ICW4);            //give info about environment
@@ -32,34 +32,36 @@ void i8259_init(void) {
 
 /* Enable (unmask) the specified IRQ */
 void enable_irq(uint32_t irq_num) {
-    uint16_t    select;         //port number for desired PIC
-    uint8_t     mask_value;     //8-bit mask register value
+    uint16_t    select;                     //port number for desired PIC
+    uint8_t     mask_prev, mask_new;        //8-bit mask register value
 
     if (irq_num<8) {
-        select = MASTER_DATA;   //IRQ is on MASTER, so select MASTER
+        select = MASTER_DATA;               //IRQ is on MASTER, so select MASTER
     } else {
-        select = SLAVE_DATA;    //IRQ is on SLAVE, so select SLAVE
-        irq_num -= 8;           //subtract 8 from irq_num for correct offset
+        select = SLAVE_DATA;                //IRQ is on SLAVE, so select SLAVE
+        irq_num -= 8;                       //subtract 8 from irq_num for correct offset
     }
 
-    mask_value = inb(select) & ~(1<<irq_num);   //make every IRQ mask except the irq_num zero
-    outb(select, mask_value);                   //ie. 0x000000X0 if irq_num is 1
+    mask_prev = inb(select);
+    mask_new = mask_prev & ~(1<<irq_num);   //unmask irq_num by making the value zero
+    outb(select, mask_new);                 //ie. 0xXXXXXX0X if irq_num is 1
 }
 
 /* Disable (mask) the specified IRQ */
 void disable_irq(uint32_t irq_num) {
-    uint16_t    select;         //port number for desired PIC
-    uint8_t     mask_value;     //8-bit mask register value
+    uint16_t    select;                     //port number for desired PIC
+    uint8_t     mask_prev, mask_new;        //8-bit mask register value
 
     if (irq_num<8) {
-        select = MASTER_DATA;   //IRQ is on MASTER, so select MASTER
+        select = MASTER_DATA;               //IRQ is on MASTER, so select MASTER
     } else {
-        select = SLAVE_DATA;    //IRQ is on SLAVE, so select SLAVE
-        irq_num -= 8;           //subtract 8 from irq_num for correct offset
+        select = SLAVE_DATA;                //IRQ is on SLAVE, so select SLAVE
+        irq_num -= 8;                       //subtract 8 from irq_num for correct offset
     }
 
-    mask_value = inb(select) & (1<<irq_num);   //make every IRQ mask except the irq_num one
-    outb(select, mask_value);                  //ie. 0x111111X1 if irq_num is 1
+    mask_prev = inb(select);
+    mask_new = mask_prev | (1<<irq_num);    //mask irq_num by making the value one
+    outb(select, mask_new);                 //ie. 0xXXXXXX1X if irq_num is 1
 }
 
 /* Send end-of-interrupt signal for the specified IRQ */
