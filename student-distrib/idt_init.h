1f39b99255be944a1e679960c64d145072bda76a
mirecki2 <mirecki2@illinois.edu>
2019-03-09 14:14:33 -0600

    Merge branch 'master' into 'mirecki2'
    # Conflicts:
    #   student-distrib/boot.S

---
+++ student-distrib/idt_init.h
@@ -0,0 +1,24 @@
+//IDT_INIT HEADER FILE
+
+void idt_init();
+void divide_error();
+void debug();
+void nmi();
+void breakpoint();
+void overflow();
+void bound_range();
+void invalid_op();
+void device_na();
+void double_fault();
+void seg_overrun();
+void invalid_tss();
+void seg_np();
+void seg_fault();
+void gen_prot();
+void page_fault();
+void blank();
+void fpe();
+void align();
+void machine();
+void simd();
+void general();
