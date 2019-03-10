dc3bb50996144ad4b93a715cb41ab4b2917283d8
Sanyal <sanyal3@illinois.edu>
2019-03-10 10:15:26 -0500

    rtc.c update

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -4,7 +4,7 @@
 
 #include "i8259.h"
 #include "lib.h"
-
+#include "types.h"
 //https://wiki.osdev.org/RTC
 
 #define RTC_MAR	0x70	//address Register / Index register
@@ -12,33 +12,34 @@
 #define RTC_IRQ_LINE 8
 
 //
-#define RTC_REGISTER_A	0x8A
-#define	RTC_REGISTER_B	0x8B
-#define	RTC_REGISTER_C	0x8C
-#define	RTC_REGISTER_D	0x8D
+#define RTC_REGISTER_A	0x0A
+#define	RTC_REGISTER_B	0x0B
+#define	RTC_REGISTER_C	0x0C
+#define	RTC_REGISTER_D	0x0D
 
-
-#define A_2_Hz 0x2F //control register A 2 Hz setting,turn oscillator on
-#define init_B 0x48 //control register B Interrupt enable setting
+#define A_turn_on 0x20 //control register A turn on osciallator
+#define A_2_Hz 0x0F //control register A 2 Hz setting
+#define init_B 0x40 //control register B Interrupt enable setting
 
 void init_rtc(void){
 
 	
 	outb( RTC_REGISTER_B,RTC_MAR);
-	char reg_b = inb(RTC_MDR);
+	uint8_t reg_b = (uint8_t)inb(RTC_MDR);
 	//toggle the interrupt enable bit
 	reg_b |= init_B;
-		outb( RTC_REGISTER_B,RTC_MAR);
+	outb( RTC_REGISTER_B,RTC_MAR);
 	outb(reg_b,RTC_MDR);
 	
 
-	
+	uint8_t reg_a = A_turn_on;
 	outb( RTC_REGISTER_A, RTC_MAR);
-	char reg_a = inb(RTC_MDR);
-	//toggle the bottome 7 bits 
-	reg_a &= 0x80;
+	outb( reg_a,RTC_MDR);
+
+	outb( RTC_REGISTER_A, RTC_MAR);
+	reg_a = (uint8_t)inb(RTC_MDR);
 	reg_a |= A_2_Hz;
-		outb( RTC_REGISTER_A, RTC_MAR);
+	outb( RTC_REGISTER_A, RTC_MAR);
 	outb( reg_a,RTC_MDR);
 	
 	
@@ -51,6 +52,6 @@ void init_rtc(void){
 void rtc_interrupt(void){
 	
 	send_eoi(RTC_IRQ_LINE);
-	printf("rtc interrupt recieved \n" );
+	putc('5');putc('\n');
 
 }
