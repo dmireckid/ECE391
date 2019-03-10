a0595af7266d0474491c17fd67b7aff02b4f4337
Hu <ch4@illinois.edu>
2019-03-10 12:07:54 -0500

    Finessing keyboard

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -7,8 +7,15 @@
 #include "i8259.h"
 
 /* maps keycode to ASCII character code */
-char keymap[256] =  {   '\0', (char)27, /* 0x00: not used, 0x01: esc key */
-                        '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' /* 0x02~0x0B: numbers 1~9 and 0 */ 
+char keymap[256] =  {   '\0', (char)27 /*0x01: escape*/,							/* 0x00: not used, 0x01: esc key */
+                        '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',			/* 0x02~0x0B: numbers 1~9 and 0 */ 
+						'-', '=', '\0' /*0x0E: backspace*/, '\0' /*0x0F: tab*/,		/* 0x0C~0x0F */
+						'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',			/* 0x10~0x19: letters q~p */
+						'[', ']', '\n' /*0x1C: enter*/, '\0' /*0x1D: left ctrl*/,	/* 0x1A~0x1D */
+						'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',				/* 0x1E~0x26: letters a~l */
+						';', '\'', '`', '\0' /*0x2A: left shift*/, '\\',			/* 0x27~0x2B */
+						'z', 'x', 'c', 'v', 'b', 'n', 'm',							/* 0x2C~0x32: letters z~m */
+						',', '.', '/',												/* 0x33~0x35 */
                     };
 
 /* enable IRQ1 to open keyboard interrupt */
