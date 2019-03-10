abafd4d798fc242c42fcbdc426aa66920ea146bb
Sanyal <sanyal3@illinois.edu>
2019-03-10 14:42:15 -0500

    rtc_asm.h

--- student-distrib/rtc_asm.h
+++ student-distrib/rtc_asm.h
@@ -7,7 +7,15 @@
 #define _RTC_ASM_H
 
 
-
+/*
+ * rtc_handler()
+ *   DESCRIPTION: this is the interrupt handler for rtc interrupts
+ *   INPUTS: none
+ *   OUTPUTS: nones
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: Saves all registers and flags, then calls the rtc_interrupt,
+ *                restores registers, then returns from interrupt 
+ */
 extern void rtc_handler();
 
 #endif
