8fa219d74143238d2f7310f21939d4e4051a12bb
Sanyal <sanyal3@illinois.edu>
2019-03-10 11:48:01 -0500

    rtc.c and lib.h modifications

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -24,6 +24,22 @@
 void init_rtc(void){
 
 	
+
+	
+	//turn on oscillator
+	uint8_t reg_a = A_turn_on;
+	outb( RTC_REGISTER_A, RTC_MAR);
+	outb( reg_a,RTC_MDR);
+
+	//set oscillator frequency
+	outb( RTC_REGISTER_A, RTC_MAR);
+	reg_a = (uint8_t)inb(RTC_MDR);
+	reg_a |= A_2_Hz;
+	outb( RTC_REGISTER_A, RTC_MAR);
+	outb( reg_a,RTC_MDR);
+	
+	
+	
 	outb( RTC_REGISTER_B,RTC_MAR);
 	uint8_t reg_b = (uint8_t)inb(RTC_MDR);
 	//toggle the interrupt enable bit
@@ -31,16 +47,6 @@ void init_rtc(void){
 	outb( RTC_REGISTER_B,RTC_MAR);
 	outb(reg_b,RTC_MDR);
 	
-
-	uint8_t reg_a = A_turn_on;
-	outb( RTC_REGISTER_A, RTC_MAR);
-	outb( reg_a,RTC_MDR);
-
-	outb( RTC_REGISTER_A, RTC_MAR);
-	reg_a = (uint8_t)inb(RTC_MDR);
-	reg_a |= A_2_Hz;
-	outb( RTC_REGISTER_A, RTC_MAR);
-	outb( reg_a,RTC_MDR);
 	
 	
 	
@@ -50,8 +56,11 @@ void init_rtc(void){
 
 
 void rtc_interrupt(void){
-	
+	//read register C so interrupt is cleared from the RTC
+	outb( RTC_REGISTER_C, RTC_MAR);
+	inb(RTC_MDR);
+	//required functionality for cp1
 	send_eoi(RTC_IRQ_LINE);
-	putc('5');putc('\n');
+	test_interrupts();
 
 }
