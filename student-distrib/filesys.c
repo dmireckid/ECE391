dca2aae0e1c3e62dbf77999b43c28fee61d3af7d
Damian Mirecki <mirecki2@illinois.edu>
2019-03-14 14:56:07 -0500

    more filesystem functions

--- student-distrib/filesys.c
+++ student-distrib/filesys.c
@@ -1,13 +1,48 @@
 
 
 int32_t read_dentry_by_name (const uint8_t* fname, dentry_t* dentry){
-	
+
 }
 
 int32_t read_dentry_by_index (uint32_t index, dentry_t* dentry){
-	
+
 }
 
 int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length){
-	
+
+}
+
+int32_t open_f(const uint8_t* fname){
+	//initialize temp structs, then return 0
+	return 0;
+}
+
+int32_t open_d(const uint8_t* fname){
+	//read_dentry_by_name
+	return 0;
+}
+
+int32_t close_f(int32_t f){
+	//undo temp structs from open_f, then return success
+	return 0;
+}
+
+int32_t close_d(int32_t d){
+	return 0;				//do nothing, "successfully" closed
+}
+
+int32_t read_f(int32_t f, int32_t count, void* buf){
+
+}
+
+int32_t read_d(int32_t d, int32_t count, void* buf){
+
+}
+
+int32_t write_f(int32_t f, int32_t count, void* buf){
+	return -1;				//read-only filesystem, cannot write
+}
+
+int32_t write_d(int32_t d, int32_t count, void* buf){
+	return -1;				//read-only filesystem, cannot write
 }
--- student-distrib/filesys.h
+++ student-distrib/filesys.h
@@ -1,5 +1,13 @@
 //FILESYS_PARSE HEADER FILE
 
-int32 t read dentry by name (const uint8 t* fname, dentry t* dentry);
-int32 t read dentry by index (uint32 t index, dentry t* dentry);
-int32 t read data (uint32 t inode, uint32 t offset, uint8 t* buf, uint32 t length);
+int32_t read_dentry_by_name (const uint8_t* fname, dentry_t* dentry);
+int32_t read_dentry_by_index (uint32_t index, dentry_t* dentry);
+int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
+int32_t open_f(const uint8_t* fname);
+int32_t open_d(const uint8_t* fname);
+int32_t close_f(int32_t f);
+int32_t close_d(int32_t d);
+int32_t read_f(int32_t f, int32_t count, void* buf);
+int32_t read_d(int32_t d, int32_t count, void* buf);
+int32_t write_f(int32_t f, int32_t count, void* buf);
+int32_t write_d(int32_t d, int32_t count, void* buf);
