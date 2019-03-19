8a3ffea2ae273bc0b4d0d4faa09fb777e8b0977c
Jason Hahm <hahm4@illinois.edu>
2019-03-19 00:39:09 -0500

    Aligned comments properly

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -22,29 +22,29 @@ char keymap[NUM_ASCII] =  {   '\0', '\0' /*0x01: escape*/,							/* 0x00: not us
 /* First column contains keycodes for symbol keys */
 /* and the second column contains ascii codes for symbols when shifted */
 char shifted_symbols_map[21][2] = {	/* symbols on number keys */
-									{0x02, '!'},     // 1 to !
-									{0x03, '@'},     // 2 to @
-									{0x04, '#'},     // 3 to #
-									{0x05, '$'},     // 4 to $
-									{0x06, '%'},     // 5 to %
-									{0x07, '^'},     // 6 to ^
-									{0x08, '&'},     // 7 to &
-									{0x09, '*'},     // 8 to *
-									{0x0A, '('},     // 9 to (
-									{0x0B, ')'},     // 0 to )
+									{0x02, '!'},	// 1 to !
+									{0x03, '@'},	// 2 to @
+									{0x04, '#'},	// 3 to #
+									{0x05, '$'},    // 4 to $
+									{0x06, '%'},    // 5 to %
+									{0x07, '^'},    // 6 to ^
+									{0x08, '&'},    // 7 to &
+									{0x09, '*'},    // 8 to *
+									{0x0A, '('},    // 9 to (
+									{0x0B, ')'},    // 0 to )
 
 									/* other symbols */
-									{0x29, '~'},    // ` to ~
-									{0x0C, '_'},     // - to _
-									{0x0D, '+'},     // = to +
+									{0x29, '~'},	// ` to ~
+									{0x0C, '_'},    // - to _
+									{0x0D, '+'},    // = to +
 									{0x1A, '{'},    // [ to {
 									{0x1B, '}'},    // ] to }
 									{0x2B, '|'},    // \ to |
-									{0x27, ':'},     // ; to :
-									{0x28, '"'},     // ' to "
-									{0x33, '<'},     // , to <
-									{0x34, '>'},     // . to >
-									{0x35, '?'}      // / to ?
+									{0x27, ':'},    // ; to :
+									{0x28, '"'},    // ' to "
+									{0x33, '<'},    // , to <
+									{0x34, '>'},    // . to >
+									{0x35, '?'}     // / to ?
 };
 
 /* flags for Shift, Control, and Caps Lock */
