a5deebf1f5603a9e5bc92d095c84efbba8f9120f
Hu <ch4@illinois.edu>
2019-04-01 11:13:18 -0500

    Saving read/write

--- student-distrib/sys_calls.c
+++ student-distrib/sys_calls.c
@@ -64,7 +64,7 @@ int32_t execute (const uint8_t* command)
     //copy program into memory
     read_data(test.inode_num,0,(uint8_t*)PROGRAM_VIRTUAL_ADDRESS,PROGRAM_SIZE);
 
-    //Initialize PCB stdin/out file descriptors
+    //Initialize PCB values and stdin/out file descriptors
     init_STD(current_pid);
     pcb_array[current_pid].parent_pid = current_pid-1;
     
@@ -103,6 +103,11 @@ void init_STD(uint32_t pid)
     pcb_array[pid].fd_array[1].fp =0;
     pcb_array[pid].fd_array[1].flags =IN_USE_FLAG;
 
+	int i;
+	for (i = 2; i < 8; i++) {
+		pcb_array[pid].fd_array[1].flags = NOT_IN_USE_FLAG;
+	}
+
     pcb_array[pid].files_in_use = 2;
 }
 
@@ -111,59 +116,20 @@ void init_STD(uint32_t pid)
  *
  *	INPUTS: int32_t fd - file descriptor
  * 			void* buf - buffer
- *			int32_t nbytes - file syze (in bytes)
+ *			int32_t nbytes - file size (in bytes)
  *	OUTPUTS: none
- *	RETURN VALUE: file descriptor
+ *	RETURN VALUE: number of bytes written, or -1 on failure
  *	SIDE EFFECTS: runs the read command based on the file type
  */
 int32_t read (int32_t fd, void* buf, int32_t nbytes)
 {
-    dentry_t test;
-    //check if file exists
-    //if(read_dentry_by_name(filename,&test)==-1) return -1;
-    switch(test.filetype)
-    {
-        case 0://rtc
-        {
-            pcb_array[current_pid].files_in_use++;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].fops = (uint32_t)rtc_jumptable;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].inode = 0;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].fp = 0;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].flags = IN_USE_FLAG;
-            break;
-        }
-        case 1://directory
-        {
-
-            pcb_array[current_pid].files_in_use++;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].fops = (uint32_t)directory_jumptable;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].inode = 0;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].fp = 0;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].flags = IN_USE_FLAG;
-            break;
-        }
-        case 2://file
-        {
-
-            pcb_array[current_pid].files_in_use++;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].fops = (uint32_t)file_jumptable;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].inode = test.inode_num;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].fp = 0;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].flags = IN_USE_FLAG;
-            break;
-        }
-        default:
-            return -1;
-
-
-    }
+    //check if file descriptor is in bounds and if the flag is IN_USE
+    if(fd > 7 || fd < 0) return -1;
+	if(pcb_array[current_pid].fd_array[fd].flags == NOT_IN_USE_FLAG) return -1;
  
-    uint32_t* ptr = (uint32_t*)pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].fops; 
+    uint32_t* ptr = (uint32_t*)pcb_array[current_pid].fd_array[fd].fops; 
     int32_t (*fun_ptr)(int32_t, const void*, int32_t) = (void*)ptr[1];
-    (*fun_ptr)(fd,buf,nbytes);
-
-    
-    return pcb_array[current_pid].files_in_use-1;
+    return (*fun_ptr)(fd,buf,nbytes);
 }
 
 /*
@@ -171,59 +137,20 @@ int32_t read (int32_t fd, void* buf, int32_t nbytes)
  *
  *	INPUTS: int32_t fd - file descriptor
  * 			void* buf - buffer
- *			int32_t nbytes - file syze (in bytes)
+ *			int32_t nbytes - file size (in bytes)
  *	OUTPUTS: none
- *	RETURN VALUE: file descriptor
+ *	RETURN VALUE: number of bytes written, or -1 on failure
  *	SIDE EFFECTS: runs the write command based on the file type
  */
 int32_t write (int32_t fd, const void* buf, int32_t nbytes)
 {
-    dentry_t test;
-    //check if file exists
-    //if(read_dentry_by_name(filename,&test)==-1) return -1;
-    switch(test.filetype)
-    {
-        case 0://rtc
-        {
-            pcb_array[current_pid].files_in_use++;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].fops = (uint32_t)rtc_jumptable;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].inode = 0;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].fp = 0;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].flags = IN_USE_FLAG;
-            break;
-        }
-        case 1://directory
-        {
-
-            pcb_array[current_pid].files_in_use++;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].fops = (uint32_t)directory_jumptable;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].inode = 0;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].fp = 0;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].flags = IN_USE_FLAG;
-            break;
-        }
-        case 2://file
-        {
-
-            pcb_array[current_pid].files_in_use++;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].fops = (uint32_t)file_jumptable;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].inode = test.inode_num;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].fp = 0;
-            pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].flags = IN_USE_FLAG;
-            break;
-        }
-        default:
-            return -1;
-
-
-    }
+    //check if file descriptor is in bounds and if the flag is IN_USE
+    if(fd > 7 || fd < 0) return -1;
+	if(pcb_array[current_pid].fd_array[fd].flags == NOT_IN_USE_FLAG) return -1;
  
-    uint32_t* ptr = (uint32_t*)pcb_array[current_pid].fd_array[pcb_array[current_pid].files_in_use-1].fops; 
+    uint32_t* ptr = (uint32_t*)pcb_array[current_pid].fd_array[fd].fops; 
     int32_t (*fun_ptr)(int32_t, const void*, int32_t) = (void*)ptr[2];
-    (*fun_ptr)(fd,buf,nbytes);
-
-    
-    return pcb_array[current_pid].files_in_use-1;
+    return (*fun_ptr)(fd,buf,nbytes);
 }
 
 /*
