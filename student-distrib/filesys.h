ec1708971485ba011e36b89f907e45f5e4817a4a
Damian Mirecki <mirecki2@illinois.edu>
2019-04-04 13:04:45 -0500

    satisfying git

--- student-distrib/filesys.h
+++ student-distrib/filesys.h
@@ -1,13 +1,39 @@
 //FILESYS_PARSE HEADER FILE
+#include "types.h"
+#include "lib.h"
 
-int32_t read_dentry_by_name (const uint8_t* fname, dentry_t* dentry);
-int32_t read_dentry_by_index (uint32_t index, dentry_t* dentry);
-int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
+#define FILENAME_LEN 32
+
+typedef struct{
+  int8_t filename[FILENAME_LEN];
+  int32_t filetype;
+  int32_t inode_num;
+  int8_t reserved[24];
+}dentry_t;    //directory entry struct
+
+typedef struct{
+  int32_t i_length;
+  int32_t data_block_num[1023];
+}inode_t;   //index node struct
+
+
+typedef struct{
+  int32_t dir_count;
+  int32_t inode_count;
+  int32_t data_count;
+  int8_t reserved[52];
+  dentry_t direntries[63];
+}bootblock_t;   //bootblock struct
+
+void init_filesystem(uint32_t address);
+int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry);
+int32_t read_dentry_by_index(uint32_t index, dentry_t* dentry);
+int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
 int32_t open_f(const uint8_t* fname);
 int32_t open_d(const uint8_t* fname);
-int32_t close_f(int32_t f);
-int32_t close_d(int32_t d);
-int32_t read_f(int32_t f, int32_t count, void* buf);
-int32_t read_d(int32_t d, int32_t count, void* buf);
-int32_t write_f(int32_t f, int32_t count, void* buf);
-int32_t write_d(int32_t d, int32_t count, void* buf);
+int32_t close_f(int32_t fd);
+int32_t close_d(int32_t fd);
+int32_t read_f(int32_t fd, uint32_t offset, void* buf,uint32_t length);
+int32_t read_d(int32_t fd, int32_t count, uint8_t* buf);
+int32_t write_f(int32_t fd, int32_t count, void* buf);
+int32_t write_d(int32_t fd, int32_t count, void* buf);
